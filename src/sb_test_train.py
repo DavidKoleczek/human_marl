ae4498c358059eebc8c60fa320064ed6c5730ec0
Siddhant Pradhan <siddhant96@gmail.com>
2021-04-22 20:50:47 -0400

    multiple trials

--- src/sb_test_train.py
+++ src/sb_test_train.py
@@ -8,7 +8,7 @@ import gym
 from stable_baselines3 import SAC
 from stable_baselines3.common.evaluation import evaluate_policy
 from eval_interventions import evaluate_policy_interventions
-
+import torch
 
 from environments.hitl_sb_lunarlandercont import HITLSBLunarLanderCont
 from environments.hitl_sb_budget_lunarlandercont import HITLSBBudgetLunarLanderCont
@@ -19,6 +19,8 @@ import sys
 
 import argparse
 
+np.random.seed(0)
+torch.manual_seed(0)
 
 def default_params():
     params = {}
@@ -26,6 +28,7 @@ def default_params():
     params['penalty'] = 1
     params['budget'] = 1000
     params['eval'] = 100
+    params['trials'] = 5
     
     return params
 
@@ -38,6 +41,7 @@ if __name__ == '__main__':
     parser.add_argument('--penalty', type=float, help='penalty per intervention')
     parser.add_argument('--budget', type = int, help='intervention budget')
     parser.add_argument('--eval', type=int, help='number of evaluation episodes')
+    parser.add_argument('--trials', type=int, help='number of trials')
     
 
     parser.set_defaults(**default_params())
@@ -50,31 +54,41 @@ if __name__ == '__main__':
     budget = args.budget
     total_time = args.total_timesteps
     eval_ep = args.eval
+    trials = args.trials
     
-    
+    rewards, lengths, interventions = [], [], []
     # env = HITLSBLunarLanderCont('LunarLanderContinuous-v2', human, intervention_penalty=penalty)
-    env = HITLSBBudgetLunarLanderCont('LunarLanderContinuous-v2', human, intervention_penalty=penalty, budget = budget)
+    for trial in range(trials):
+        print('Trial: {}'.format(trial+1))
+        env = HITLSBBudgetLunarLanderCont('LunarLanderContinuous-v2', human, intervention_penalty=penalty, budget = budget)
 
-    model = SAC('MlpPolicy', env, verbose=1, tensorboard_log='runs')
+        model = SAC('MlpPolicy', env, verbose=1, tensorboard_log='runs')
 
-    log_name = 'SAC_ip={}_human={}'.format(penalty, 'Sensor0.1')
-    model.learn(total_timesteps=total_time, tb_log_name=log_name, callback=TensorboardCallback())
-    model.save('../savedModels/sac_lunar_hitl_{}p_{}b_sensor01.zip'.format(penalty, budget))
+        log_name = 'SAC_ip={}_human={}'.format(penalty, 'Sensor0.1')
+        model.learn(total_timesteps=total_time, tb_log_name=log_name, callback=TensorboardCallback())
+        model.save('../savedModels/sac_lunar_hitl_{}p_{}b_sensor01.zip'.format(penalty, budget))
 
-    # Evaluate the trained agent
-    # eval_env = HITLSBLunarLanderCont('LunarLanderContinuous-v2', human)
-    eval_env = HITLSBBudgetLunarLanderCont('LunarLanderContinuous-v2', human)
-
-    # mean_reward, std_reward = evaluate_policy(model, eval_env, n_eval_episodes=eval_ep, deterministic=True)
-    rewards, lengths, interventions = evaluate_policy_interventions(model, eval_env, n_eval_episodes=eval_ep, deterministic=True, return_episode_rewards= True)
+        # Evaluate the trained agent
+        # eval_env = HITLSBLunarLanderCont('LunarLanderContinuous-v2', human)
+        eval_env = HITLSBBudgetLunarLanderCont('LunarLanderContinuous-v2', human)
+        
+        # mean_reward, std_reward = evaluate_policy(model, eval_env, n_eval_episodes=eval_ep, deterministic=True)
+        r, l, i = evaluate_policy_interventions(model, eval_env, n_eval_episodes=eval_ep, deterministic=True, return_episode_rewards= True)
+        rewards.extend(r)
+        lengths.extend(l)
+        interventions.extend(i)
+    
     mean_reward = np.mean(rewards)
     std_reward = np.std(rewards)
     mean_int = np.mean(interventions)
     std_int = np.std(interventions)
+    mean_length = np.mean(lengths)
+    std_length = np.std(lengths)
     
     violations = [1 for i in interventions if i > budget]
     
     print(f'mean_reward={mean_reward:.2f} +/- {std_reward}')
     print(f'mean_intervention={mean_int:.2f} +/- {std_int}')
-    print('Violations = {}/{}'.format(sum(violations),eval_ep))
+    print(f'mean_episode_length={mean_length:.2f} +/- {std_length}')
+    print('Violations = {}/{}'.format(sum(violations),eval_ep*trials))
     print(penalty, budget)
