1d124e9dc2260bb5d0447ae46461d2c405ec5c7f
Dave <45405824+DavidKoleczek@users.noreply.github.com>
2021-05-18 12:17:25 -0400

    adapt job

--- experiments/lander_sac_adapt.py
+++ experiments/lander_sac_adapt.py
@@ -1,6 +1,6 @@
 import os
 import csv
-from multiprocessing import Pool
+import argparse
 
 from stable_baselines3 import SAC
 
@@ -14,23 +14,51 @@ from src.utils.eval_interventions import evaluate_policy_interventions
 from src.utils.tensorboard_callback import TensorboardCallback
 
 
-intervention_rate = 0.5
+def human_experiment(human_params):
+    # we need an optimal agent to base our simulated agents
+    if not os.path.exists('./saved_models/lander_sac_optimal.zip'):
+        human = train_optimal_agent()
+    else:
+        human = SAC.load('./saved_models/lander_sac_optimal.zip')
 
-human = SAC.load('./saved_models/lander_sac_optimal.zip')
-human = SensorAgent(human, 0.1)
+    # create a simulated human agent
+    human_parameter = human_params[1]
+    human_agent = human_params[0](human, human_parameter)
 
-env = HITLLanderContinuousAdapt('LunarLanderContinuous-v2', human,
-                                intervention_rate=intervention_rate, initial_intervention_penalty=1)
-model = SAC('MlpPolicy', env, verbose=1, tensorboard_log='runs')
+    intervention_rates = [0.1, 0.25, 0.5, 0.75]
+    # run 10 trials for each hyperparameter setting
+    for _ in range(1):
+        for rate in intervention_rates:
+            env = HITLLanderContinuousAdapt('LunarLanderContinuous-v2', human,
+                                            intervention_rate=rate, initial_intervention_penalty=1)
+            model = SAC('MlpPolicy', env, verbose=1, tensorboard_log='runs')
 
-log_name = 'SACadapt={}_human={}'.format(intervention_rate, 'Sensor0.1')
-model.learn(total_timesteps=325000, tb_log_name=log_name, callback=TensorboardCallback())
-model.save('./saved_models/lander_sac/lander_{}.zip'.format(log_name))
+            log_name = 'SACadapt={}_human={}'.format(rate, 'Sensor0.1')
+            output_path = './results/landersacadapt_' + human_agent.__class__.__name__ + '.csv'
+
+            model.learn(total_timesteps=2500, tb_log_name=log_name, callback=TensorboardCallback())
+            model.save('./saved_models/lander_sac/lander_{}.zip'.format(log_name))
+
+            # evaluate the trained agent
+            eval_env = HITLLanderContinuousAdapt('LunarLanderContinuous-v2', human)
+            mean_reward, mean_int, mean_eplen, mean_int_rate = evaluate_policy_interventions(
+                model, eval_env, n_eval_episodes=5, deterministic=True, return_episode_rewards=False)
+
+            # save the results for each trial to a csv file
+            os.makedirs(os.path.dirname(output_path), exist_ok=True)
+            with open(output_path, 'a', newline='') as csvfile:
+                csvwriter = csv.writer(csvfile)
+                csvwriter.writerow([rate, mean_reward, mean_int, mean_eplen, mean_int_rate])
+
+    return None
 
 
-# Evaluate the trained agent
-eval_env = HITLLanderContinuousAdapt('LunarLanderContinuous-v2', human)
-mean_reward, mean_int, mean_eplen, mean_int_rate = evaluate_policy_interventions(
-    model, eval_env, n_eval_episodes=100, deterministic=True, return_episode_rewards=False)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("human_number", type=int)
+    args = parser.parse_args()
 
-print(mean_reward, mean_int, mean_eplen, mean_int_rate)
+    human_number = args.human_number
+    humans = [(SensorAgent, 0.1), (LaggyAgent, 0.8), (NoisyAgent, 0.15)]
+
+    human_experiment(humans[human_number])
